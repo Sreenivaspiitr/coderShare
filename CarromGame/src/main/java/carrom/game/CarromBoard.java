3284a46cdc8218b29d85344cbc23cf0c523d01ba
Sreenivas <sreenivaspiitr@gmail.com>
2019-03-23 12:45:35 +0530

    added additional test case and use case: adding white coins

--- CarromGame/src/main/java/carrom/game/CarromBoard.java
+++ CarromGame/src/main/java/carrom/game/CarromBoard.java
@@ -7,10 +7,13 @@ import carrom.game.utils.Player;
 import static carrom.game.utils.CarromGameConstants.*;
 
 public class CarromBoard {
-	 private Player player1;
+	 private static final String WHITE_MULTI_STRIKE = "White MultiStrike";
+	private static final String WHITE_STRIKE = "White Strike";
+	private Player player1;
 	 private Player player2;
 	 private int black;
-	private int red;
+	 private int red;
+	 private int white;
 	 private boolean game = true;
 	 private boolean result = false;
 	 private Player winner;
@@ -22,6 +25,7 @@ public class CarromBoard {
 		 this.player2 = p2;
 		 black = 9;
 		 red = 1;
+		 white = 5;
 	 }
 	 
 	 public int getBlack() {
@@ -35,31 +39,62 @@ public class CarromBoard {
 	 public void startGame() {
 		 System.out.println("Starting Clean Strike game :");
 		 Scanner input = new Scanner(System.in);
-		 int option = 0;
+		 int option = 0;boolean valid = true;
 		 
 		 while(game && !result) {
+			 if(valid) {
 			 System.out.println("Player 1 \"" + player1.getName() + "\" : Choose an outcome from the list below \n1. Strike \n2. Multistrike "
-			 		+ "\n3. Red strike \n4. Striker strike \n5. Defunct coin \n6. None");
+			 		+ "\n3. Red strike \n4. Striker strike \n5. Defunct coin \n6. None \n7. WhiteStrike \n8. White MultiStrike");
 			 option = input.nextInt();   // check for valid input range..
+			 valid = validateInput(option);
+			 if(!valid) {
+				 System.out.println("Please enter valid input");
+				 valid = true;continue;}
+			 
 			 processGame(option,player1);
 			 
 			 checkGame();
+			 }
 			 
 			 if(!game || result)break;
 			 
+			 valid = true;
+			 if(valid) {
 			 System.out.println("Player 2 \"" + player2.getName() + "\" : Choose an outcome from the list below \n1. Strike \n2. Multistrike "
-				 		+ "\n3. Red strike \n4. Striker strike \n5. Defunct coin \n6. None");
+				 		+ "\n3. Red strike \n4. Striker strike \n5. Defunct coin \n6. None  \n7. White Strike \n8. White MultiStrike");
 			 option = input.nextInt();
+			 valid = validateInput(option);
+			 if(!valid) {
+				 System.out.println("Please enter valid input");
+				 continue;
+			 }
+			 
 			 processGame(option,player2);
 			 
 			 checkGame();
-			 
+			 }
 			 System.out.println("Score board : " + player1.getName() + " - "+ player1.getScore()
 			 + " & " + player2.getName() + " - "+ player2.getScore());
 		 }
 		 
 		 System.out.println("Clean Strike game is finished!");
 			 
+	 }
+	 
+	 public boolean validateInput(int opt) {
+		 if(opt<0 || opt >8)return false;
+		 
+		 if(black<=0) {
+			 if(opt<3)return false;
+		 }
+		 if(red <=0 && opt==3)return false;
+		 
+		 if(white<=0) {
+			 if(opt>6)return false;
+		 }
+		 
+		 
+		 return true;
 	 }
 	 
 	 public void processGame(int opt, Player player) {
@@ -94,6 +129,16 @@ public class CarromBoard {
 			 player.setScore(0); //null strike
 			 player.setMove(NONE);
 			 break;
+		 case 7 : 
+			 player.setScore(2); //null strike
+			 player.setMove(WHITE_STRIKE);
+			 white--;
+			 break;
+		 case 8 : 
+			 player.setScore(4); //null strike
+			 player.setMove(WHITE_MULTI_STRIKE);
+			 white -= 2;
+			 break;
 		 default :
 			 player.setScore(0);
 			 break;
@@ -103,8 +148,9 @@ public class CarromBoard {
 	 }
 	 
 	 public void checkGame() {
-//		 System.out.println("Checking game..");
-		 if(black<=0 && red<=0)game = false;
+//		 if(black <= 0 && red>0)
+//		 System.out.println("Blacks are finsihed, only red strikes are possible..");
+		 if(black<=0 && red<=0 && white <=0)game = false;
 		 
 		 if(player1.getScore()<5 && player2.getScore()<5)return;
 		 
@@ -155,7 +201,7 @@ public class CarromBoard {
 			 System.out.print(move + "  ");
 		 }
 		 List<String> history2 = player2.getMoves();
-		 System.out.println(player2.getName() + " - ");
+		 System.out.println("\n" + player2.getName() + " - ");
 		 for(String move : history2) {
 			 System.out.print(move + "  ");
 		 }
--- CarromGame/src/test/java/CarromGameTest.java
+++ CarromGame/src/test/java/CarromGameTest.java
@@ -4,16 +4,30 @@ import org.junit.Test;
 
 import carrom.game.CarromBoard;
 import carrom.game.utils.Player;
+import static carrom.game.utils.CarromGameConstants.*;
 
 public class CarromGameTest {
-	
+	private CarromBoard cb;
+		
 	@Test
 	public void testProcessGame() {
 		Player pl = new Player("Vasu",11);
-		CarromBoard cb = new CarromBoard(pl,pl);
+		cb = new CarromBoard(pl,pl);
 		cb.processGame(1, pl);
 		
 		assertEquals(cb.getBlack(),8);
 		assertEquals(pl.getScore(),1);
+	}
+	
+	@Test
+	public void testCheckThreeFoul() {
+		Player pl = new Player("Vasu",11);
+		pl.setMove(STRIKER_STRIKE);
+		pl.setMove(DEFUNCT_COIN);
+		pl.setMove(STRIKER_STRIKE);
+		
+		cb = new CarromBoard(pl,pl);
+		
+		assertEquals(cb.checkThreeFoul(pl),true);
 	}
 }
